b746a518963899a23b40163ba312dd08bbc43aaa
Ave-de-Rapina <jhschmidt001@gmail.com>
2022-11-09 04:54:33 -0300

    Validacao do formulario login js - admin@admin.com ; admin

---
+++ js/Home-login.js
@@ -0,0 +1,14 @@
+
+function logar(){
+    var login = document.getElementById('email').value;
+    var senha = document.getElementById('senha').value;
+    
+    if(login == "admin@admin.com" && senha == "admin"){
+        location.href = "../html/Home-produtos.html";
+        
+    }else{
+        alert("Usuario ou senha incorretos.")
+    }
+    
+}
+
